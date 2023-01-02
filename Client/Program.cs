97fdcd70d870d94ed3e66a6acd33a5b79f70856a
eking <elijahgideonking@gmail.com>
2023-01-02 09:43:14 -0500

    add client product service

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,8 @@
+global using BlazorEcommerce.Shared;
+global using System.Net.Http.Json;
+
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
